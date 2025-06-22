1cef05dc965b5b4c0d755cd8d356b4b8207f4bb5
Divyakanth Reddy <44220908+DivyakanthReddy123@users.noreply.github.com>
2025-06-22 16:25:22 -0400

    Time: 0 ms (100.00%) | Memory: 12.6 MB (5.32%) - LeetSync

--- 61-rotate-list/rotate-list.py
+++ 61-rotate-list/rotate-list.py
@@ -12,15 +12,18 @@ class Solution(object):
         curr = head 
         n = 0
 
+        # this problem is similar to the remove nth node from the linkedlist !! 
+        # but here we are doing k%n because k might be large than n also 
+        # but here we go till the last node not further above it to the None as in the prev problem
         if not head or not head.next or k == 0:
             return head
 
-
+        # to count the number of Nodes in the LINKEDLIST 
         while curr : 
             curr = curr.next 
             n+=1  
 
-
+        # when this is the case we get the same linkedlist if after rotating it 
         if k == n :
             return head 
         
@@ -33,13 +36,14 @@ class Solution(object):
             if fast.next :  
                 fast = fast.next 
         
+        # but here we go till the last node not further above it to the None as in the prev problem
         while fast.next :
             fast = fast.next 
             slow = slow.next 
 
         
-        new_head = slow.next # new node 4 
-        fast.next = head 
+        new_head = slow.next # just to save the START POINT of the new LINKEDLIST  
+        fast.next = head  # point the last node in the previous LINKEDLIST to the head of the new LINKEDLIST 
         slow.next = None 
         
         return new_head 
