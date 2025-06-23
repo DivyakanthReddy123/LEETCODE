1b33093f23cf5d2d59db9be790d83275902a792f
Divyakanth Reddy <44220908+DivyakanthReddy123@users.noreply.github.com>
2025-06-23 00:55:14 -0400

    Time: 56 ms (74.29%) | Memory: 22.1 MB (14.17%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        # when sum goes negative .. then start from 0 and keep on comparing with 
+        # previous max val 
+
+        if len(nums) == 1 :
+            return nums[0]
+
+        currSum = 0 
+        maxSum = nums[0]
+
+        for num in nums :
+            # currSum += num # this line should be below the if statement 
+            # because , then we get the true max subarry 
+            if currSum < 0 :
+                currSum = 0
+            currSum += num 
+            maxSum = max (maxSum , currSum)
+
+        return maxSum 
