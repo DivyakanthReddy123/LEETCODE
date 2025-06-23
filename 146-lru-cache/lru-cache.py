1c10ff48609709e8fb57478ac3a452c76cfa6f79
Divyakanth Reddy <44220908+DivyakanthReddy123@users.noreply.github.com>
2025-06-23 10:58:21 -0400

    Time: 108 ms (87.49%) | Memory: 79.5 MB (6.63%) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,40 @@
+
+    # we use Ordered Dictionary for special features 
+
+        # 3 cases here .. 
+        # 1 -> if it exists move to the end for mostly recently  used !! and then update it  
+        # 2 -> if not in cache then add it 
+        # 3 -> if the capacity exceeds then remove the least recently used 
+        # using the .popitem
+      
+from collections import OrderedDict
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            # Mark as recently used
+            self.cache.move_to_end(key)
+            return self.cache[key]
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            # Mark as recently used
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            # Pop the least recently used (first) item
+            self.cache.popitem(last=False)  
+
+
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
